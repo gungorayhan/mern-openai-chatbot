910b8f9155761dc15714cf86208e6812a67bdfa7
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2023-12-21 13:01:03 +0300

    model

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,9 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.getAllUsers = void 0;
-const getAllUsers = (req, res) => {
-    //get all users here
-    res.send("getAllUsers");
+const User_1 = __importDefault(require("../models/User"));
+const getAllUsers = async (req, res) => {
+    try {
+        const users = await User_1.default.find();
+        return res.status(200).json({
+            message: "OK",
+            users
+        });
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "ERROR",
+            cause: error.message
+        });
+    }
 };
 exports.getAllUsers = getAllUsers;
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,18 @@
 import {Request,Response} from "express"
-export const getAllUsers = (req:Request,res:Response) => {
-    //get all users here
+import User from "../models/User";
 
-    res.send("getAllUsers")
+export const getAllUsers = async(req:Request,res:Response) => {
+   try {
+    const users = await User.find();
+    return res.status(200).json({
+        message:"OK",
+        users
+    })
+   } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+        message:"ERROR",
+        cause:error.message
+    })
+   }
 }
